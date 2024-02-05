33f0bb7562a6f730319bf72d20609485c9e9ca01
Jared Hancock <hancock1@kenyon.edu>
2024-02-05 10:11:17 -0500

    update

--- SearchPrefix.rb
+++ SearchPrefix.rb
@@ -1,5 +1,12 @@
 def searchPrefix(arr,s)
-
+    inhash = []
+    l = s.length
+    arr.each do |w|
+        if w[0..(l-1)] == s
+            inhash.push(w)
+        end
+    end
+    inhash
 end
 
 print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"ab"))
